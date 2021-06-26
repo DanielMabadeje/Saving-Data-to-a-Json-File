cf27b01960ccb1930fb658cf49b67e9625a106e2
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-26 08:44:44 -0700

    edited db.json

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@
                 
                 <div class="container pt-5">
                     <div class="col-md-12 pt-5">
-                        <div class="card  col-md-12">
+                        <div class="card  col-md-12 pb-4">
                             <h2 class="pt-5">Add Product</h2>
                             <div class="form mt-5 ">
                                 <form class="">
@@ -53,35 +53,25 @@
 
 
 
-                        <div class="col-md-12">
+                        <div class="col-md-12 card mt-5">
                             <table class="table">
                                 <thead>
                                   <tr>
                                     <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                    <th scope="col">Product Name</th>
+                                    <th scope="col">Quantity In Stock</th>
+                                    <th scope="col">Price Per Item</th>
+                                    <th scope="col">Total Value Number</th>
                                   </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="" id="products">
                                   <tr>
                                     <th scope="row">1</th>
                                     <td>Mark</td>
                                     <td>Otto</td>
-                                    <td>@mdo</td>
+                                    <td>fs</td>
                                   </tr>
-                                  <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                  </tr>
-                                  <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                  </tr>
+                                  
                                 </tbody>
                               </table>
                         </div>
